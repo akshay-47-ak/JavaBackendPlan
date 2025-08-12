bed992a56bfb1f786c52787b5e5728b2ec0de74c
Akshay <akshayathavale1525@gmail.com>
2025-08-12 20:24:24 +0530

    Update Project

---
+++ src/main/java/com/learn/backend/com/learn/JDBC/App.java
@@ -0,0 +1,13 @@
+package com.learn.backend.com.learn.JDBC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
